65de6ac0575a551e0e06a1472db91696b5d26fad
longtn <longtn@seatechit.com.vn>
2021-06-15 23:55:43 +0700

    cmimit

--- CutLink/src/main/java/SpringBot/demo/ConnectDB.java
+++ CutLink/src/main/java/SpringBot/demo/ConnectDB.java
@@ -6,8 +6,33 @@ import java.util.Date;
 import java.util.List;
 public class ConnectDB {
     private Connection con;
-    private static Statement st;
+    public Connection getCon() {
+		return con;
+	}
+
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+
+	public static Statement getSt() {
+		return st;
+	}
+
+	public static void setSt(Statement st) {
+		ConnectDB.st = st;
+	}
+
+	public static ResultSet getRs() {
+		return rs;
+	}
+
+	public static void setRs(ResultSet rs) {
+		ConnectDB.rs = rs;
+	}
+
+	private static Statement st;
     private static ResultSet rs;
+    
     
     public ConnectDB(){
     try{
@@ -26,10 +51,12 @@ public class ConnectDB {
     		String sql = "select address from tbl_info where id_key ="+key+" and status = 1 and Ngay_tao <= ENDDATE";
         	rs = st.executeQuery(sql);
         	
+        	
         	 while(rs.next()){
         		 adr = rs.getString("address");
                  
              }
+        	 st.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
@@ -40,10 +67,38 @@ public class ConnectDB {
     {
     	String adr="";
     	try {
-    		String sql = "update tbl_info set address = "+adress+" where id_key ="+key;
-        	 st.executeUpdate(sql);
-        	 
+    		java.util.Date date=new java.util.Date();
+    		java.sql.Date sqlDate=new java.sql.Date(date.getTime());
+    		String sql = "update tbl_info set address = ? , Ngay_tao = ? where id_key ="+key +" and Ngay_tao< ?";
+    		PreparedStatement ps=con.prepareStatement(sql);
+    		ps.setString(1,adress);
+    		ps.setDate(2,sqlDate);
+    		ps.setDate(3,sqlDate);
+    		ps.executeUpdate();
+    		
+    		ps.close();
         	
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("loi update");
+			// TODO: handle exception
+		}
+    	return adr;
+    }
+    
+    public String insert(String key,String adress)
+    {
+    	String adr="";
+    	try {
+    		java.util.Date date=new java.util.Date();
+    		java.sql.Date sqlDate=new java.sql.Date(date.getTime());
+    		String sql = "insert into tbl_info (id_key, address, Ngay_tao, status,ENDDATE) VALUES ("+key+",?,?,0,?)";
+    		PreparedStatement ps=con.prepareStatement(sql);
+    		ps.setString(1,adress);
+    		ps.setDate(2,sqlDate);
+    		ps.setDate(3,sqlDate);
+    		ps.executeUpdate();
+    		ps.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("loi update");
--- CutLink/src/main/java/SpringBot/demo/UpLoadController.java
+++ CutLink/src/main/java/SpringBot/demo/UpLoadController.java
@@ -1,5 +1,6 @@
 package SpringBot.demo;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,30 +22,28 @@ import com.google.gson.GsonBuilder;
 
 @RestController
 public class UpLoadController {
-	public static JFrame mainFrame;
 
 	@RequestMapping(value = "/upload", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	private String hello(@RequestBody String req, HttpServletRequest request, HttpServletResponse resp) {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		ConnectDB con = new ConnectDB();
-		
+
 		subMitClass subMitClass = gson.fromJson(req, subMitClass.class);
-		
+
 		String adress = con.getAdress(subMitClass.getKey());
-		String dressSubmid=subMitClass.getAddress();
-		String[] adressAR=adress.split(",");
-		boolean check=true;
+		String dressSubmid = subMitClass.getAddress();
+		String[] adressAR = adress.split(",");
+		boolean check = true;
 		for (int i = 0; i < adressAR.length; i++) {
-			
-			if(!dressSubmid.toLowerCase().contains(adressAR[i].toLowerCase())) {
-				check=false;
-			}else {
-				check=true;
+
+			if (!dressSubmid.toLowerCase().contains(adressAR[i].toLowerCase())) {
+				check = false;
+			} else {
+				check = true;
 			}
 		}
-		if(check==false)
-		{
-			return"";
+		if (check == false) {
+			return "";
 		}
 		List<Image> lstNew = new ArrayList<Image>();
 		List<Image> lst = subMitClass.getLstImage();
@@ -57,6 +56,15 @@ public class UpLoadController {
 			lstNew.add(image);
 		}
 		subMitClass.setLstImage(lstNew);
+		if(con.getCon()!=null)
+		{
+			try {
+				con.getCon().close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		return gson.toJson(subMitClass);
 
 	}
@@ -68,26 +76,102 @@ public class UpLoadController {
 		subMitClass subMitClass = gson.fromJson(req, subMitClass.class);
 		con.update(subMitClass.getKey(), subMitClass.getAddress());
 		String adress = con.getAdress(subMitClass.getKey());
-		String dressSubmid=subMitClass.getAddress();
-		String[] adressAR=adress.split(",");
-		boolean check=true;
-		for (int i = 0; i < adressAR.length; i++) {
+		String dressSubmid = subMitClass.getAddress();
+		if (adress != null && !adress.isEmpty()) {
+				String[] adressAR = adress.split(",");
+				boolean check = true;
+				for (int i = 0; i < adressAR.length; i++) {
+
+					if (!dressSubmid.toLowerCase().contains(adressAR[i].toLowerCase())) {
+						check = false;
+					} else {
+						check = true;
+					}
+				}
+				if (check == false) {
+					return "";
+				} else {
+
+				}
+				subMitClass newClass = new subMitClass();
+				newClass.setKey("00");
+				if(con.getCon()!=null)
+				{
+					try {
+						con.getCon().close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				return gson.toJson(newClass);
 			
-			if(!dressSubmid.toLowerCase().contains(adressAR[i].toLowerCase())) {
-				check=false;
-			}else {
-				check=true;
+
+		}
+		
+		if(con.getCon()!=null)
+		{
+			try {
+				con.getCon().close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
-		if(check==false)
-		{
-			return"";
-		}else {
-			
-		}
-		subMitClass newClass= new  subMitClass();
+		return "";
+
+	}
+
+	@RequestMapping(value = "/insert", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	private String insert(@RequestBody String req, HttpServletRequest request, HttpServletResponse resp) {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		ConnectDB con = new ConnectDB();
+		subMitClass subMitClass = gson.fromJson(req, subMitClass.class);
+		con.insert(subMitClass.getKey(), subMitClass.getAddress());
+		subMitClass newClass = new subMitClass();
 		newClass.setKey("00");
-		 return gson.toJson(newClass);
+		if(con.getCon()!=null)
+		{
+			try {
+				con.getCon().close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return gson.toJson(newClass);
+
+	}
+
+	// @RequestMapping(value = "/updatetimeadress", consumes =
+	// MediaType.APPLICATION_JSON_VALUE, produces =
+	// MediaType.APPLICATION_JSON_VALUE)
+	// private String updatetimeadress(@RequestBody String req, HttpServletRequest
+	// request, HttpServletResponse resp) {
+	// Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	// ConnectDB con = new ConnectDB();
+	// subMitClass subMitClass = gson.fromJson(req, subMitClass.class);
+	// con.update(subMitClass.getKey(), subMitClass.getAddress());
+	// subMitClass newClass= new subMitClass();
+	// newClass.setKey("00");
+	// return gson.toJson(newClass);
+	//
+	// }
+	//
+
+/*	@RequestMapping(value = "/insert")
+	private String insert() {
+		ConnectDB con = new ConnectDB();
+		con.insert("123", "123");
+		return "";
+
+	}*/
+
+	@RequestMapping(value = "/test")
+	private String test() {
+		ConnectDB con = new ConnectDB();
+		con.update("470259770", "123");
+		return "";
 
 	}
 }
